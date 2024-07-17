a0f04ffe35a17862c8286f888f1d3e58c339d9cd
Henry Lopes <hlopes-az204@outlook.com>
2024-07-17 08:27:11 -0300

    Save changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavComponent]
+  imports: [RouterOutlet, NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -27,13 +23,5 @@ export class AppComponent implements OnInit {
     if(!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
   }
 }
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  http = inject(HttpClient);
+  registerMode = false;
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }  
+
+  registerToggle() {
+    this.registerMode = !this.registerMode
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+}
