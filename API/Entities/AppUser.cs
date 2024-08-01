afa9465cdfc841233b69ee73f7baafd6a49fae29
Henry Lopes <hlopes-az204@outlook.com>
2024-07-31 21:24:40 -0300

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,11 +19,11 @@ public class AppUser
     public string? LookingFor { get; set; }
     public required string City { get; set; }
     public required string Country { get; set; }
-    public List<Photo> Photos { get; set; }
+    public List<Photo> Photos { get; set; } = [];
 
-    public int GetAge()
-    {
-        return DateOfBirth.CanculateAge();
+    // public int GetAge()
+    // {
+    //     return DateOfBirth.CanculateAge();
 
-    }
+    // }
 }
