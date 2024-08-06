b5fe38ce494f3107a8216992bdcc232fb62c412a
Henry Lopes <hlopes-az204@outlook.com>
2024-08-05 23:51:40 -0300

    End of Section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
+import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
+  
 })
 export class MemberListComponent implements OnInit{
   private memberService = inject(MembersService);
