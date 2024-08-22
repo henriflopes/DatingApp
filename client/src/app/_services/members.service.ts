bc890ea433ecd773fe229957cd1cd35fe912e381
Henry Lopes <hlopes-az204@outlook.com>
2024-08-22 19:37:19 -0300

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { asyncScheduler, map, scheduled, tap } from 'rxjs';
+import { Photo } from '../_models/photo';
 
 
 @Injectable({
@@ -36,5 +37,35 @@ export class MembersService {
     );
   }
 
- 
+  setMainPhoto(photo: Photo)
+  {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update( members => members.map( m => {
+          if (m.photos.includes(photo)) {
+            m.photoUrl = photo.url
+          }
+          
+          return m;
+        }))
+      })
+    );
+  }
+
+  deletePhoto(photo: Photo)
+  {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+      tap(() => {
+        this.members.update( members => members.map( m => {
+          if (m.photos.includes(photo)) {
+            m.photos = m.photos.filter( x => x.id !== photo.id)
+          }
+          
+          return m;
+        }))
+      })
+
+    );
+  }
+
 }
