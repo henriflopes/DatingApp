2dd3b1abf76b70ef7c63841cc393463fb17c6f2c
Henry Lopes <hlopes-az204@outlook.com>
2024-09-26 21:00:43 -0300

    End of section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,7 +33,8 @@ namespace API.Controllers
             {
                 Username = registerDto.Username,
                 Token = tokenService.GetToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -62,6 +63,7 @@ namespace API.Controllers
                 Username = user.UserName,
                 KnownAs = user.KnownAs,
                 Token = tokenService.GetToken(user),
+                Gender = user.Gender,
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
